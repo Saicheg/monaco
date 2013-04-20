c0c1326dafbb7a0f87c6e5c59c30621b4782903d
Valentine Zavadsky <saicheg@gmail.com>
2013-04-20 22:37:31 +0300

    fix index page

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@
 # Per-page layout changes:
 #
 # With no layout
-page "/index.html.erb", :layout => false
+page "index.html", :layout => false
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
